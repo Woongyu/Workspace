8d1bcff9dbab8b9f9f27118bfd67edd7d75d6974
HB04-19 <HB04-19@HB04-16>
2017-07-07 09:42:07 +0900

    Second commit

--- 0413_MyGuestBook_Dao/src/com/hb/mybatis/DAO.java
+++ 0413_MyGuestBook_Dao/src/com/hb/mybatis/DAO.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 
-//DB 관련된 처리하는 클래스
+//DB 愿��젴�맂 泥섎━�븯�뒗 �겢�옒�뒪
 public class DAO {
 	private static SqlSession ss;
 	
-	//null일 때 새로 만들어주고 아닐땐 return으로 계속 유지시켜줌.
-	//싱글턴 패턴; 프로그램이 끝날 때까지 해당 객체는 하나만 가지고 사용
+	//null�씪 �븣 �깉濡� 留뚮뱾�뼱二쇨퀬 �븘�땺�븧 return�쑝濡� 怨꾩냽 �쑀吏��떆耳쒖쨲.
+	//�떛湲��꽩 �뙣�꽩; �봽濡쒓렇�옩�씠 �걹�궇 �븣源뚯� �빐�떦 媛앹껜�뒗 �븯�굹留� 媛�吏�怨� �궗�슜
 	private synchronized static SqlSession getSql(){
 		if(ss == null){
 			ss = DBService.getFactory().openSession(false);
@@ -17,31 +17,31 @@ public class DAO {
 		return ss;
 	}
 	
-	//list 처리하는 메소드
+	//list 泥섎━�븯�뒗 硫붿냼�뱶
 	public static List<VO> getList(){
 		return  getSql().selectList("list");
 	}
 	
-	//insert 처리하는 메소드
+	//insert 泥섎━�븯�뒗 硫붿냼�뱶
 	public static int getInsert(VO vo){
 		int result = getSql().insert("insert", vo);
 		ss.commit();
 		return result;
 	}
 	
-	//상세보기 처리하는 메소드
+	//�긽�꽭蹂닿린 泥섎━�븯�뒗 硫붿냼�뱶
 	public static VO getOneList(String idx){
 		return getSql().selectOne("onelist", idx);
 	}
 	
-	//update 처리하는 메소드
+	//update 泥섎━�븯�뒗 硫붿냼�뱶
 	public static int getUpdate(VO vo){
 		int result = getSql().update("update", vo);
 		ss.commit();
 		return result;
 	}
 
-	//삭제 처리하는 메소드
+	//�궘�젣 泥섎━�븯�뒗 硫붿냼�뱶
 	public static int getDelete(String idx){
 		int result = getSql().delete("delete", idx);
 		ss.commit();
